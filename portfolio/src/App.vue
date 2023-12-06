5e42f44fcba647bdace527cff05a967146c7eb57
Paula Aschendorf <paula.aschendorf@gmail.com>
2023-12-06 12:35:08 +0100

    adds basic header layout and reset.css

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -11,6 +11,7 @@ export default {
 };
 </script>
 <style>
+@import url("./assets/reset.css");
 @import url("./assets/base.css");
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600&display=swap");
 #app {
