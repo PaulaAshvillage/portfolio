41bd2496711a073efc850ae7ad79b3a7ac1bccde
Paula Aschendorf <paula.aschendorf@gmail.com>
2023-12-06 21:38:01 +0100

    adds basic home view styling

--- portfolio/src/views/HomeView.vue
+++ portfolio/src/views/HomeView.vue
@@ -2,30 +2,58 @@
   <div class="home-container">
     <h1>Welcome to my portfolio <span id="dot">.</span></h1>
     <div class="link-container">
-      <router-link class="shaped-link" to="/projects">projects</router-link>
-      <router-link class="shaped-link" to="/about">about</router-link>
+      <router-link id="project-shape" class="shaped-link" to="/projects"
+        >projects
+      </router-link>
+      <router-link id="about-shape" class="shaped-link" to="/about"
+        >about
+      </router-link>
     </div>
   </div>
 </template>
 <style scoped>
 .home-container {
   display: flex;
+  margin-top: 20%;
+  align-items: center;
 }
 h1 {
-  font-size: 96px;
+  font-size: 78px;
 }
 #dot {
   color: var(--color-purple);
 }
 .link-container {
+  margin-right: 10%;
   display: flex;
+  gap: 2rem;
   flex-direction: column;
-  justify-content: space-between;
 }
-.shaped-link {
-  padding-block: 2rem;
-  padding-inline: 2rem;
-  background-color: blue;
+
+#project-shape {
+  color: var(--color-beige);
+  font-weight: 600;
+  font-size: 1.2rem;
+  background-color: var(--color-orange);
+  height: 180px;
+  width: 180px;
+  border-radius: 180px 0 0 0;
+  display: flex;
+  align-items: flex-end;
+  justify-content: end;
+}
+
+#about-shape {
+  color: var(--color-beige);
+  font-weight: 600;
+  font-size: 1.2rem;
+  background-color: var(--color-purple);
+  height: 180px;
+  width: 180px;
+  border-radius: 0 180px 0 0;
+  display: flex;
+  align-items: flex-end;
+  justify-content: start;
 }
 </style>
 <script></script>
